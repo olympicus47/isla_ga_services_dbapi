26cab42845f08e224e7228fea8038e66e63585a9
olympicus47 <olympicus47@gmail.com>
2022-06-28 17:52:36 +0300

    fucking varrnode generated app had routing promblems i couldn't solve

--- app/lib/logger.js
+++
@@ -1,6 +0,0 @@
-const bunyan = require('bunyan');
-
-module.exports = bunyan.createLogger({
-    "name": "dbapi",
-    "level": 10,
-});
--- app/middlewares/response/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    errors: (err, key) => {
-        const errors = err[key];
-        if (Array.isArray(errors) && errors.length) {
-            return errors[0].message || errors[0];
-        }
-    },
-    'msg, message': (err, key) => err[key],
-};
